3aa7610e69ec78d3e15c75f8b26109c4208a5f10
paul wisehart <paul@oldcode.org>
2019-11-12 09:13:14 -0500

    WIP gtkplot draw widget

--- src/main.c
+++ src/main.c
@@ -1,23 +1,30 @@
+#include <stdlib.h>
 #include <gtk/gtk.h>
 
 GtkWidget      *g_lbl_hello;
 GtkWidget      *g_lbl_count;
+static cairo_surface_t *surface = NULL;
 
 int
 main(int argc, char *argv[])
 {
 	GtkBuilder     *builder;
 	GtkWidget      *window;
+	char	       *builder_file = "glade/gtkplot.glade";
+	GError	       *err = NULL;
 
 	gtk_init(&argc, &argv);
 
 	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder, "glade/gtkplot.glade", NULL);
-
+	/* gtk_builder_add_from_file(builder, "glade/gtkplot.glade", NULL); */
+	if (gtk_builder_add_from_file(builder, builder_file, &err) == 0) {
+		fprintf(stderr, "Error adding builder from file %s\n%s\n",
+			builder_file, err->message);
+		return EXIT_FAILURE;
+	}
 	window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
 	gtk_builder_connect_signals(builder, NULL);
 
-	/* get pointers to the two labels */
 	g_lbl_hello = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_hello"));
 	g_lbl_count = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_count"));
 
@@ -29,6 +36,16 @@ main(int argc, char *argv[])
 	return 0;
 }
 
+static void
+clear_surface(void)
+{
+	cairo_t	       *cr;
+	cr = cairo_create(surface);
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_paint(cr);
+	cairo_destroy(cr);
+}
+
 void
 on_btn_hello_clicked()
 {
@@ -36,7 +53,7 @@ on_btn_hello_clicked()
 	static unsigned int count = 0;
 	char		str_count[30] = {0};
 
-	gtk_label_set_text(GTK_LABEL(g_lbl_hello), "Hello, world!");
+	gtk_label_set_text(GTK_LABEL(g_lbl_hello), "Clicked Count");
 	count++;
 	sprintf(str_count, "%d", count);
 	gtk_label_set_text(GTK_LABEL(g_lbl_count), str_count);
